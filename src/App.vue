083d892ee0da09e6154820ec5df6f08664402649
Maximo Signiorini <maximosigniorini97@gmail.com>
2020-12-20 18:31:59 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Todos,
+    AddTodo,
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+  height: 100vh;
+  background-color: #4158d0;
+  background-image: linear-gradient(
+    43deg,
+    #4158d0 0%,
+    #c850c0 46%,
+    #ffcc70 100%
+  );
+  background-repeat: no-repeat;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: rgb(255, 198, 75);
+  color: rgb(0, 0, 0);
+  padding: 7px 20px;
+  cursor: pointer;
+  outline: 0;
+}
+
+.btn:hover {
+  background: rgb(255, 188, 44);
 }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="header">
+        <h1>Todo List</h1>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+
+.header{
+    /* background: rgb(247, 0, 255); */
+    color: rgb(0, 0, 0);
+    text-align: center;
+    padding: 10px;
+    outline: 0;
+}
+
+.header a{
+    color: #fff;
+    padding-right: 5px;
+    outline: 0;
+}
+</style>
